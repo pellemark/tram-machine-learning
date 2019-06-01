b7e7a1ad4702496ae4964356c180c7a9a74007d5
Zsolt Fekete <zsolt.fekete@antavo.com>
2019-06-01 17:25:04 +0200

    MVP 2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,14 +6,14 @@ var videos = [
     // "videos/3.mp4",
     "videos/4.mp4",
     "videos/5.mp4",
-    "videos/6.mp4",
+    // "videos/6.mp4",
     // "videos/7.mp4",
     "videos/8.mp4",
     "videos/9.mp4",
     "videos/10.mp4",
     "videos/11.mp4",
     "videos/12.mp4",
-    "videos/13.mp4",
+    // "videos/13.mp4",
     "videos/14.mp4",
 ];
 
@@ -114,25 +114,25 @@ jQuery(function () {
             const y = prediction.bbox[1];
             const width = prediction.bbox[2];
             const height = prediction.bbox[3];
-            ctx.strokeStyle = "rgb(255, 234, 167)";
+            ctx.strokeStyle = "rgb(116, 185, 255)";
             ctx.lineWidth = 2;
             ctx.strokeRect(x, y, width, height);
             // Label background
-            ctx.fillStyle = "rgb(255, 234, 167)";
+            ctx.fillStyle = "rgb(116, 185, 255)";
             const textWidth = ctx.measureText(prediction.class).width;
             const textHeight = parseInt(font, 10); // base 10
             ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
 
             ctx.beginPath();
             ctx.rect(x, y, width, height);
-            ctx.fillStyle = "rgba(255, 234, 167, 0.3)";
+            ctx.fillStyle = "rgba(116, 185, 255, 0.3)";
             ctx.fill();
         });
 
         predictions.forEach(prediction => {
             const x = prediction.bbox[0];
             const y = prediction.bbox[1];
-            ctx.fillStyle = "#000000";
+            ctx.fillStyle = "#fff";
             ctx.fillText(prediction.class, x, y);
         });
 
@@ -143,18 +143,23 @@ jQuery(function () {
         ctx.lineTo(videoContainerWidth / 3 * 2 - 100, videoContainerHeight / 2);
         ctx.lineTo(videoContainerWidth - 150, videoContainerHeight);
 
+
         var totalArea = ((videoContainerWidth + videoContainerWidth / 3 - 200) * videoContainerHeight / 2) / 2;
         var jointPixels = calculateJointPixels(ctx, predictions);
 
         var warningScore = jointPixels / totalArea * 100;
 
         if (warningScore > 8) {
-            ctx.fillStyle = "rgba(214, 48, 49, 0.2)";
+            ctx.fillStyle = "rgba(214, 48, 49, 0.35)";
             // setSpeed(speed - warningScore);
+        } else if (warningScore > 4) {
+            ctx.fillStyle = "rgba(225, 112, 85, 0.30)";
         } else if (warningScore > 0) {
-            ctx.fillStyle = "rgba(253, 203, 110, 0.2)";
+            ctx.fillStyle = "rgba(253, 203, 110, 0.25)";
+        } else if (warningScore > -4) {
+            ctx.fillStyle = "rgba(85, 239, 196, 0.30)";
         } else {
-            ctx.fillStyle = "rgba(0, 184, 148, 0.2)";
+            ctx.fillStyle = "rgba(0, 184, 148, 0.35)";
         }
 
         ctx.fill();
